f142bb07f940f997df422b7625a8a639c6470741
Micalhl <micalhl@sina.cn>
2023-06-19 21:55:34 +0800

    [Dev] Code Cleanup

--- src/main/kotlin/me/icebear03/splendidenchants/enchant/data/BasicData.kt
+++ src/main/kotlin/me/icebear03/splendidenchants/enchant/data/BasicData.kt
@@ -2,8 +2,10 @@ package me.icebear03.splendidenchants.enchant.data
 
 import org.bukkit.NamespacedKey
 
-data class BasicData(val id: String,
-                     val name: String,
-                     val maxLevel: Int) {
+data class BasicData(
+    val id: String,
+    val name: String,
+    val maxLevel: Int
+) {
     val key: NamespacedKey = NamespacedKey.fromString(id, null) ?: error("minecraft")
 }
