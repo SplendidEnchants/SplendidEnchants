27569e367de3bf6866c57ada1f7c8f0c5f532ab3
Micalhl <micalhl@sina.cn>
2023-06-20 22:22:19 +0800

    [Dev] Introducing YamlUpdater

---
+++ src/main/kotlin/me/icebear03/splendidenchants/util/YamlUpdater.kt
@@ -0,0 +1,35 @@
+package me.icebear03.splendidenchants.util
+
+import taboolib.common.platform.function.releaseResourceFile
+import taboolib.module.configuration.Configuration
+import taboolib.module.configuration.Type
+import java.nio.charset.StandardCharsets
+
+/**
+ * SplendidEnchants
+ * me.icebear03.splendidenchants.util.YamlUpdater
+ *
+ * @author mical
+ * @since 2023/6/20 9:38 PM
+ */
+object YamlUpdater {
+
+    fun loadAndUpdate(path: String, whitelist: List<String>): Configuration {
+        val resource = YamlUpdater::class.java.getResourceAsStream(path) ?: return Configuration.empty()
+        val source = resource.readBytes().toString(StandardCharsets.UTF_8)
+        val sourceConfig = Configuration.loadFromString(source, Type.YAML)
+        val file = releaseResourceFile(path)
+        val config = Configuration.loadFromFile(file)
+        sourceConfig.getKeys(false).forEach {
+            if (it in whitelist && config[it] != sourceConfig[it]) {
+                config[it] = sourceConfig[it]
+            }
+        }
+        config.saveToFile(file)
+        return config
+    }
+}
+
+fun Configuration.loadAndUpdate(path: String, whitelist: List<String>): Configuration {
+    return YamlUpdater.loadAndUpdate(path, whitelist)
+}
