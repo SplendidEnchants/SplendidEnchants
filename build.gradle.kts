250ca0b0a040b070d607c48482000b3256c7c856
Micalhl <micalhl@xina.cn>
2023-07-11 20:02:42 +0800

    [Project] Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `java-library`
     `maven-publish`
     id("io.izzel.taboolib") version "1.56"
-    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
 }
 
 taboolib {
