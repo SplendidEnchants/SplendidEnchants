1b9e0c04b19515897016e38504ce408a9ee0cf99
Micalhl <micalhl@sina.cn>
2023-07-18 15:13:56 +0800

    问题修复: 修复了村民交易附魔问题

--- src/main/kotlin/world/icebear03/splendidenchants/PapiExpansion.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/PapiExpansion.kt
@@ -3,10 +3,12 @@ package world.icebear03.splendidenchants
 import org.bukkit.entity.Player
 import taboolib.platform.compat.PlaceholderExpansion
 
-class PapiExpansion(override val identifier: String) : PlaceholderExpansion {
-    override fun onPlaceholderRequest(player: Player?, holder: String): String {
-        if (player == null)
-            return holder
-        return holder
+object PapiExpansion : PlaceholderExpansion {
+
+    override val identifier: String
+        get() = "splendidenchants"
+
+    override fun onPlaceholderRequest(player: Player?, args: String): String {
+        return args
     }
 }
--- src/main/kotlin/world/icebear03/splendidenchants/listener/mechanism/VillagerListener.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/listener/mechanism/VillagerListener.kt
@@ -1,6 +1,7 @@
 package world.icebear03.splendidenchants.listener.mechanism
 
 import org.bukkit.event.entity.VillagerAcquireTradeEvent
+import org.bukkit.inventory.MerchantRecipe
 import taboolib.common.platform.event.EventPriority
 import taboolib.common.platform.event.SubscribeEvent
 import world.icebear03.splendidenchants.api.EnchantAPI
@@ -23,24 +24,31 @@ object VillagerListener {
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     fun event(event: VillagerAcquireTradeEvent) {
-        val recipe = event.recipe
-        val result = recipe.result
+        val result = event.recipe.result.clone()
 
         if (ItemAPI.getEnchants(result).isEmpty())
             return
-
         if (!enableEnchantTrade) {
             event.isCancelled = true
             return
         }
 
         val item = ItemAPI.clearEnchants(result)
+
         for (i in 0 until amount) {
             val enchant = EnchantAPI.drawInRandom(EnchantGroup.getSplendidEnchants(tradeEnchantGroup)) ?: return
             ItemAPI.addEnchant(item, enchant, enchant.maxLevel)
         }
 
-        //日了，改不了
-        // recipe.result=item
+        val recipe = MerchantRecipe(
+            item,
+            event.recipe.uses,
+            event.recipe.maxUses,
+            event.recipe.hasExperienceReward(),
+            event.recipe.villagerExperience,
+            event.recipe.priceMultiplier
+        )
+        recipe.ingredients = event.recipe.ingredients
+        event.recipe = recipe
     }
 }
