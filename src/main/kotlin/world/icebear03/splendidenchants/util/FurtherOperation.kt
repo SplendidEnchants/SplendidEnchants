3bb6cd875c4fde5bd94a7ec950bfb5d22ec057b6
HamsterYDS <1294243258@qq.com>
2023-07-18 22:46:08 +0800

    FurtherOperation

--- src/main/kotlin/world/icebear03/splendidenchants/util/FurtherOperation.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/util/FurtherOperation.kt
@@ -1,7 +1,9 @@
 package world.icebear03.splendidenchants.util
 
+import org.bukkit.block.Block
 import org.bukkit.entity.Entity
 import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
 
 object FurtherOperation {
 
@@ -9,6 +11,14 @@ object FurtherOperation {
     fun furtherDamage(damager: Entity, damaged: LivingEntity, damage: Double) {
         if (PermissionChecker.hasDamagePermission(damager, damaged)) {
             damaged.damage(damage, damager)
+        }
+    }
+
+    //后面两个参数因附魔而异
+    //比如较大面积的一次性破坏方块，triggereffects应该为false，否则会导致玩家卡顿
+    fun furtherBreak(player: Player, block: Block, triggerEffects: Boolean = true, dropExperience: Boolean = true) {
+        if (PermissionChecker.hasBlockPermission(player, block)) {
+            block.breakNaturally(player.inventory.itemInMainHand, triggerEffects, dropExperience)
         }
     }
 }
