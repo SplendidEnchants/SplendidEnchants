1ff91ec22dff3031c66c39e136ff2214e8882fd9
HamsterYDS <1294243258@qq.com>
2023-07-20 22:59:25 +0800

    filter完善

--- src/main/kotlin/world/icebear03/splendidenchants/SplendidEnchants.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/SplendidEnchants.kt
@@ -6,6 +6,7 @@ import world.icebear03.splendidenchants.enchant.EnchantGroup
 import world.icebear03.splendidenchants.enchant.EnchantLoader
 import world.icebear03.splendidenchants.enchant.data.Rarity
 import world.icebear03.splendidenchants.enchant.data.Target
+import world.icebear03.splendidenchants.util.EnchantFilter
 
 object SplendidEnchants : Plugin() {
 
@@ -18,8 +19,9 @@ object SplendidEnchants : Plugin() {
         Target.initialize()
         EnchantLoader.initialize()
         EnchantGroup.initialize()
-//        println("${System.currentTimeMillis()}3+7-a*9.5".compileToJexl().eval(mapOf("a" to 6)))
-        // 注意需提前缓存好，建议在加载附魔的时候就缓存好编译好的公式，因为编译有损耗
+
+
+        EnchantFilter.test()
     }
 
     override fun onDisable() {
--- src/main/kotlin/world/icebear03/splendidenchants/util/EnchantFilter.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/util/EnchantFilter.kt
@@ -6,56 +6,98 @@ import world.icebear03.splendidenchants.enchant.EnchantLoader
 import world.icebear03.splendidenchants.enchant.SplendidEnchant
 import world.icebear03.splendidenchants.enchant.data.Rarity
 import world.icebear03.splendidenchants.enchant.data.Target
+import world.icebear03.splendidenchants.util.EnchantFilter.FilterStatement.OFF
+import world.icebear03.splendidenchants.util.EnchantFilter.FilterStatement.ON
 import world.icebear03.splendidenchants.util.EnchantFilter.FilterType.*
 import java.util.*
 
 object EnchantFilter {
 
     val all = EnchantLoader.enchantByName.values
-    val filterMap = mutableMapOf<UUID, MutableMap<Pair<FilterType, Any>, FilterStatement>>()
-
-    fun test() {
-        println(
-            filter(
-                mutableMapOf(
-                    (FilterType.RARITY to Rarity.fromIdOrName("传奇")) to FilterStatement.ON
-                )
-            )
-        )
-    }
-
+    val filterMap = mutableMapOf<UUID, MutableMap<FilterType, MutableList<Pair<Any, FilterStatement>>>>()
 
     fun filter(player: Player): List<SplendidEnchant> {
         createIfNotExists(player)
         return filter(filterMap[player.uniqueId]!!)
     }
 
-    fun filter(filters: MutableMap<Pair<FilterType, Any>, FilterStatement>): List<SplendidEnchant> {
+    fun filter(filters: MutableMap<FilterType, MutableList<Pair<Any, FilterStatement>>>): List<SplendidEnchant> {
         return all.filter { enchant ->
-            filters.filter {
-                val type = it.key.first
-                val value = it.key.second
-                val state = it.value
+            println("--------------------------")
+            var flag = true
 
-                val isConsistent = when (type) {
-                    RARITY -> enchant.rarity == (value as Rarity)
-                    TARGET -> enchant.targets.contains(value as Target)
-                    TYPE -> EnchantGroup.isIn(enchant, value.toString())
-                    STRING -> enchant.basicData.name.contains(value.toString()) ||
-                            enchant.displayer.generalDescription.contains(value.toString())
+            filters.forEach {
+                var onFlag = false
+                var offFlag = false
+
+                var onExists = false
+                it.value.forEach { pair ->
+                    if (pair.second == ON)
+                        onExists = true
                 }
 
-                if (state == FilterStatement.ON)
-                    isConsistent
-                else
-                    !isConsistent
+                it.value.forEach { pair ->
+                    run {
+                        val result = when (it.key) {
+                            RARITY -> pair.first as Rarity == enchant.rarity
+                            TARGET -> enchant.targets.contains(pair.first as Target)
+                            TYPE -> EnchantGroup.isIn(enchant, pair.first.toString())
+                            STRING -> {
+                                println(enchant.basicData.name + " " + pair.first.toString())
+                                enchant.basicData.name.contains(pair.first.toString()) ||
+                                        enchant.basicData.id.contains(pair.first.toString()) ||
+                                        enchant.displayer.generalDescription.contains(pair.first.toString())
+                            }
+                        }
+                        if (result) {
+                            when (pair.second) {
+                                ON -> onFlag = true
+                                OFF -> offFlag = true
+                            }
+                            println(enchant.basicData.name + "满足：" + it.key + " 这是" + pair.second)
+                        }
+                    }
+                }
+
+                if (!onExists)
+                    onFlag = true
+
+                println("       $onFlag $offFlag")
+                if (!onFlag || offFlag)
+                    flag = false
             }
-            true
+            flag
         }
     }
 
+    fun generateLore(type: FilterType, player: Player): List<String> {
+        if (!filterMap.containsKey(player.uniqueId))
+            return listOf()
+        val lore = mutableListOf<String>()
+        val pairs = filterMap[player.uniqueId]!![type] ?: return listOf()
+
+        pairs.forEach {
+            lore += it.second.symbol + " " + when (type) {
+                RARITY -> {
+                    val rarity = it.first as Rarity
+                    rarity.color + rarity.name
+                }
+
+                TARGET -> {
+                    val target = it.first as Target
+                    target.name
+                }
+
+                TYPE, STRING -> {
+                    it.first
+                }
+            }
+        }
+        return lore
+    }
+
     fun createIfNotExists(player: Player) {
-        if (filterMap.containsKey(player.uniqueId))
+        if (!filterMap.containsKey(player.uniqueId))
             filterMap[player.uniqueId] = mutableMapOf()
     }
 
@@ -71,40 +113,25 @@ object EnchantFilter {
             TYPE -> value
             STRING -> value
         }
-        filterMap[player.uniqueId]!!.filter { it.key.second == key }.forEach {
-            filterMap[player.uniqueId]!!.remove(it.key)
-        }
-        filterMap[player.uniqueId]!![type to key] = state
+        if (!filterMap[player.uniqueId]!!.containsKey(type))
+            filterMap[player.uniqueId]!![type] = mutableListOf()
+        filterMap[player.uniqueId]!![type]!! += key to state
     }
 
     fun removeFilter(player: Player, type: FilterType) {
         createIfNotExists(player)
-        filterMap[player.uniqueId]!!.filter { it.key.first == type }.forEach {
-            filterMap[player.uniqueId]!!.remove(it.key)
-        }
+        filterMap[player.uniqueId]!![type] = mutableListOf()
     }
 
-    enum class FilterType(displayName: String) {
+    enum class FilterType(val displayName: String) {
         RARITY("品质"),
         TARGET("物品类别"),
         TYPE("类型/定位"),
         STRING("名字/描述");
-
-        val displayName: String
-
-        init {
-            this.displayName = displayName
-        }
     }
 
-    enum class FilterStatement(symbol: String) {
-        ON("√"),
-        OFF("×");
-
-        val symbol: String
-
-        init {
-            this.symbol = symbol
-        }
+    enum class FilterStatement(val symbol: String) {
+        ON("§7√"),
+        OFF("§c×");
     }
 }
