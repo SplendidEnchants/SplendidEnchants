d4ee503853a4e62912859a71dad7ab3e1fd97740
Daniel Zhang <68374769+xiaozhangup@users.noreply.github.com>
2023-09-25 23:24:24 +0800

    新增 menu 命令选项

--- src/main/kotlin/world/icebear03/splendidenchants/command/impl/CommandMenu.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/command/impl/CommandMenu.kt
@@ -7,24 +7,43 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import taboolib.common.platform.command.SimpleCommandBody
 import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.command.suggest
 import taboolib.common.platform.command.suggestPlayers
 import world.icebear03.splendidenchants.command.CommandHandler
+import world.icebear03.splendidenchants.ui.AnvilUI
+import world.icebear03.splendidenchants.ui.EnchantSearchUI
+import world.icebear03.splendidenchants.ui.ItemCheckUI
 import world.icebear03.splendidenchants.ui.MainMenuUI
 
 object CommandMenu : CommandExecutor {
 
+    private val menus = listOf("main", "anvil", "search", "check")
+
     override val command: SimpleCommandBody
         get() = subCommand {
-            execute<CommandSender> { sender, args, _ -> handle(sender, null) }
+            execute<CommandSender> { sender, _, _ -> handle(sender) }
             dynamic("player", true) {
                 suggestPlayers()
                 execute<CommandSender> { sender, args, _ -> handle(sender, args["player"]) }
+
+                dynamic(optional = true) {
+                    suggest { menus }
+
+                    execute<CommandSender> { sender, args, menu ->
+                        handle(sender, menu, args["player"])
+                    }
+                }
             }
         }
 
-    fun handle(sender: CommandSender, who: String?) {
+    fun handle(sender: CommandSender, menu: String = "main", who: String? = null) {
         (who?.let { Bukkit.getPlayer(it) } ?: (sender as? Player))?.let { receiver ->
-            MainMenuUI.open(receiver)
+            when(menu) {
+                "main" -> MainMenuUI.open(receiver)
+                "anvil" -> AnvilUI.open(receiver)
+                "search" -> EnchantSearchUI.open(receiver)
+                "check" -> ItemCheckUI.open(receiver, mode = ItemCheckUI.CheckMode.FIND)
+            }
         } ?: sender.sendLang("command.subCommands.menu.fail")
     }
 
