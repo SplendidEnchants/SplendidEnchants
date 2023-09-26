5f2caec3733a1a68bbac8dee2eca8e00f4ac8c9b
Daniel Zhang <68374769+xiaozhangup@users.noreply.github.com>
2023-09-26 22:33:34 +0800

    修真特殊显示问题

--- src/main/kotlin/world/icebear03/splendidenchants/enchant/EnchantDisplayer.kt
+++ src/main/kotlin/world/icebear03/splendidenchants/enchant/EnchantDisplayer.kt
@@ -71,8 +71,8 @@ object EnchantDisplayer {
         if (!combineMode) lore += enchants.map { (enchant, level) -> enchant.displayer.display(level, player, item) }
         else {
             val enchantPairs = enchants.filter { !separateSpecial || it.key.displayer.isDefaultDisplay() }.toList()
-            for (i in 0 until enchants.size step amount) {
-                val total = min(amount, enchants.size - i)
+            for (i in enchantPairs.indices step amount) {
+                val total = min(amount, enchantPairs.size - i)
                 var layout = layouts[total - 1]
                 for (j in 0 until total) {
                     val enchantPair = enchantPairs[i + j]
